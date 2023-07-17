4dbc55f29f7b33f224ef2a06d6173bd74d0f299d
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-17 15:22:39 -0300

    add

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,10 +1,16 @@
-import './style.css';
-
+import './video.css';
+import { BoxVideo } from './style';
 
 export function Video(){
     return(
-        <div className='videoApresentacao'>
-            <h1>Conheça nosso método!</h1>
-        </div>
+        <BoxVideo>
+
+            <div className='videoApresentacao'>
+                <h1>Conheça nosso método!</h1>
+                <div className='videoAqui'>
+                </div>
+                <button className='btn'>EU QUERO!</button>
+            </div>
+        </BoxVideo>
     )
 }
