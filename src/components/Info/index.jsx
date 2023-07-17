cbfa8d3a69a9886a27491fa6ab93bb972225e646
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-17 08:49:24 -0300

    add infor

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -6,7 +6,19 @@ export function Info() {
 
     return(
         <div className='informacoes'>
-            <h1>tesdte</h1>
+            <h1>Porque nosso método <br/> é diferenciado?</h1>
+            <div className='skills'>
+                <section>
+
+                </section>
+                <section>
+
+                </section>
+                <section>
+                    
+                </section>
+
+            </div>
 
         </div>
     )
