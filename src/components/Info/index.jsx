d34385ee5d981f685a07cbe294d7ae2468a9603b
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-17 12:35:14 -0300

    add grid

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -1,5 +1,9 @@
 import './style.css';
-
+import aprovado from '../../assets/verificado.png'
+import leitura from '../../assets/leitura-agil.png'
+import conteudo from '../../assets/conteudo-atrativo.png'
+import revisao from '../../assets/revisao.png'
+import experiencia from '../../assets/experiencia.png'
 
 
 export function Info() {
@@ -9,14 +13,29 @@ export function Info() {
             <h1>Porque nosso método <br/> é diferenciado?</h1>
             <div className='skills'>
                 <section>
-
+                    <img src={aprovado} alt="" />
+                    <h3>Material aprovado</h3>
                 </section>
                 <section>
-
+                    <img src={leitura} alt="" />
+                    <h3>Material aprovado</h3>
                 </section>
+                
                 <section>
-                    
+                    <img src={conteudo} alt="" />
+                    <h3>Material aprovado</h3>
                 </section>
+                
+                <section>
+                    <img src={revisao} alt="" />
+                    <h3>Material aprovado</h3>
+                </section>
+                
+                <section>
+                    <img src={experiencia} alt="" />
+                    <h3>Material aprovado</h3>
+                </section>
+                
 
             </div>
 
