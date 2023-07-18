16f53671935133bc1c67af11997c2e1b02a51a6b
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-18 15:19:53 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import { Depoimentos } from './components/Depoimentos'
+
 import { Info } from './components/Info'
 import { Topo } from './components/Topo'
 import { Video } from './components/Video'
@@ -14,6 +15,7 @@ export function App() {
       <Info/>
       <Video/>
       <Depoimentos/>
+      
     </>
   )
 }
