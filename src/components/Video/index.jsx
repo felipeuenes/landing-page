b56d14cccd55e7bf450d20e0d941c53610bc73bc
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-18 15:07:59 -0300

    add link

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -14,7 +14,10 @@ export function Video(){
                 <iframe src="https://www.youtube.com/embed/jix4JBDV3WE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
                 </div>
       {/* </ScrollRevealContainer> */}
+      <a href="https://wa.me/5588996857750?text=Ol%C3%A1%2C+gostaria+de+saber+mais+sobre+o+m%C3%A9todo+Aprenda+J%C3%A1%21" target="_blank" rel="noopener noreferrer">
+
                 <button className='btn'>EU QUERO!</button>
+      </a>
             </div>
        
     )
