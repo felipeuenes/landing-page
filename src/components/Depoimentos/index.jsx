89d86c635e6f472782efd3b8c70e88fbd27b7b0d
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-20 20:52:07 -0300

    teste

--- src/components/Depoimentos/index.jsx
+++ src/components/Depoimentos/index.jsx
@@ -14,10 +14,10 @@ export function Depoimentos() {
         <div className="depoimentosBox">
             <h1>Depoimentos</h1>
             <div className="container">
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
+                <div className="card">teste</div>
+                <div className="card">teste</div>
+                <div className="card">teste</div>
+                <div className="card">teste</div>
                 
                 
                 
