4b2d45551f71b281b3f9ef206653fe8616f4f42f
Felipe Uenes <uenesdesigner@gmail.com>
2023-07-20 15:05:53 -0300

    add depoimentos

--- src/components/Depoimentos/index.jsx
+++ src/components/Depoimentos/index.jsx
@@ -9,6 +9,15 @@ export function Depoimentos() {
 
         <div className="depoimentosBox">
             <h1>Depoimentos</h1>
+            <div className="container">
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+                
+                
+                
+            </div>
         </div>
         </ScrollRevealContainer>
     )
